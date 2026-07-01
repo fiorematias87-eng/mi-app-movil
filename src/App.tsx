46ec6970a0e8ae9b725517ff3547c0566c1631e1
Matias <fiorematias87@gmail.com>
2026-07-01 16:20:21 +0000

    app lista para render

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeCliente from './views/client/HomeCliente';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 md:py-8">
+      <HomeCliente />
+    </div>
+  );
+}
